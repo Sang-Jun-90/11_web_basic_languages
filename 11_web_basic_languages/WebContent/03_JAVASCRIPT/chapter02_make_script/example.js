7937623378117db77407af48880a326ee9b637dd
19시_웹개발_LSJ <19__LSJ@DESKTOP-VD237BJ>
2021-04-09 21:46:25 +0900

    1

---
+++ 11_web_basic_languages/WebContent/03_JAVASCRIPT/chapter02_make_script/example.js
@@ -0,0 +1,9 @@
+	document.write("<h1>Welcome to javascript</h1>");
+	document.write("hellow javascript1");
+	document.write("hellow javascript1<br>");
+	document.write("<hr>");
+
+	console.log("hello javascript1");
+	console.log("hello javascript2");
+	console.log("<h1>hello javascript</h1>");
+	
